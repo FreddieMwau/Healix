b54776aa7cb3b1057caf4fcdccfe63e49afbb00b
FreddieMwau <fredrickmutua12@gmail.com>
2023-06-27 11:38:25 +0300

    feature: profile page design complete

--- app/src/main/java/com/thinkdev/healix/activity/ProfileActivity.java
+++ app/src/main/java/com/thinkdev/healix/activity/ProfileActivity.java
@@ -7,11 +7,15 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
 import com.thinkdev.healix.R;
+import com.thinkdev.healix.adapter.ProfileTabAdapter;
 import com.thinkdev.healix.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ProfileTabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,33 @@ public class ProfileActivity extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        tabLayout = findViewById(R.id.profileTabLayout);
+        viewPager2 = findViewById(R.id.profileViewPager);
+        tabAdapter = new ProfileTabAdapter(this);
+        viewPager2.setAdapter(tabAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 }
