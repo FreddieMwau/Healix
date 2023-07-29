8534d2cd39c0749c92a6c8768b2022e51306b2bf
FreddieMwau <fredrickmutua12@gmail.com>
2023-07-29 19:46:38 +0300

    Services list (#5)
    * fix: Edit app launcher icon
    
    * fix: bug fix on the services event listener
    
    * feature: Add delete, edit bottom sheets & service bottom sheet

--- app/src/main/java/com/thinkdev/healix/adapter/ServicesChildAdapter.java
+++ app/src/main/java/com/thinkdev/healix/adapter/ServicesChildAdapter.java
@@ -3,7 +3,6 @@ package com.thinkdev.healix.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,52 +10,53 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.thinkdev.healix.R;
 import com.thinkdev.healix.interfaces.ServiceInterface;
+import com.thinkdev.healix.interfaces.TransactionInterface;
 import com.thinkdev.healix.model.ServicesChildModel;
+import com.thinkdev.healix.model.ServicesModel;
 
 import java.util.List;
 
-public class ServicesChildAdapter extends RecyclerView.Adapter<ServicesChildAdapter.ServicesViewHolder> {
-
-    private List<ServicesChildModel> servicesChildModels;
+public class ServicesChildAdapter extends RecyclerView.Adapter<ServicesChildAdapter.ServicesChildViewHolder> {
+    private List<ServicesChildModel> servicesModelList;
     private ServiceInterface serviceInterface;
 
-    public ServicesChildAdapter(List<ServicesChildModel> servicesChildModels) {
-        this.servicesChildModels = servicesChildModels;
+    public ServicesChildAdapter(List<ServicesChildModel> servicesModelList) {
+        this.servicesModelList = servicesModelList;
     }
 
     @NonNull
     @Override
-    public ServicesChildAdapter.ServicesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ServicesChildAdapter.ServicesChildViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.services_card_design, parent, false);
-        return new ServicesChildAdapter.ServicesViewHolder(view);
+        return new ServicesChildViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ServicesChildAdapter.ServicesViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ServicesChildAdapter.ServicesChildViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return servicesChildModels.size();
+        return servicesModelList.size();
     }
 
-    public void setOnItemClickListener(ServiceInterface serviceInterface) {
-        this.serviceInterface = serviceInterface;
+    public void setOnItemClickListener(ServiceInterface serviceInterfaces) {
+        this.serviceInterface = serviceInterfaces;
     }
 
-    public class ServicesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class ServicesChildViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView title, description, time, amount;
 
-        ImageView icon;
-        TextView title, description, amount, time;
-        public ServicesViewHolder(@NonNull View itemView) {
+        public ServicesChildViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            amount = itemView.findViewById(R.id.serviceAmount);
             title = itemView.findViewById(R.id.serviceTitle);
             description = itemView.findViewById(R.id.serviceDescription);
-            amount = itemView.findViewById(R.id.serviceAmount);
             time = itemView.findViewById(R.id.serviceTime);
+            itemView.setOnClickListener(this);
         }
-
 
         @Override
         public void onClick(View v) {
